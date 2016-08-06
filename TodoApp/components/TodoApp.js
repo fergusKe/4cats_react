f3bccc86a8cb0d89fae1569408292cf4e5ca2eea
fergus <fergus@yahoo.com.tw>
2016-08-06 14:16:05 +0800

    level-17_container-pattern modify

--- TodoApp/components/TodoApp.js
+++ TodoApp/components/TodoApp.js
@@ -1,10 +1,15 @@
 const {
+	TodoActions,
 	TodoHeaderContainer,
 	CreateTodoFieldContainer,
 	TodoListContainer
 } = window.App;
 
 class TodoApp extends React.Component {
+	componentDidMount() {
+		TodoActions.loadTodos();
+	}
+	
 	render() {
 		return (
 			<div>
